3eea3f04d38d3ed66f9526f81039c89cead5e271
OccupyMars2025 <1945340016@qq.com>
2024-11-28 12:17:42 +0800

    delete Verilator-generated files

--- Design-a-RISC-V-CPU/step-03-design-a-multi-cycle-RISC-V-CPU/synthesizable/obj_dir/test_cpu.d
+++
@@ -1,6 +0,0 @@
-test_cpu.o: ../test_cpu.cpp \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h VRISCVCPU.h
